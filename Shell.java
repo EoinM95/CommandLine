3d21c33f6b1d8cb43a99f1a615b4b8429195ef93
Eoin Murphy <murphe43@tcd.ie>
2015-12-18 19:51:05 +0100

    Updated comments in Shell, removed thread.isAlive()

--- Shell.java
+++ Shell.java
@@ -12,8 +12,7 @@ public class Shell implements Runnable {
 		}
 		
 		public void kill(){
-			this.interrupt();
-			
+			//this.interrupt();
 		}
 
 		/**
@@ -67,16 +66,6 @@ public class Shell implements Runnable {
 		try{
 			while(true){
 				ShellThread current = null;
-				//Thread.sleep(20);
-				/*for(int i=0;i<processCount;i++){
-					ShellThread p=activeProcesses.get((Integer)i);
-					if(p!=null&&!p.isAlive()){
-						System.out.println("La commande pid="+i+" a terminé, son resultat:");
-						System.out.println(p.getCommand().result());
-						activeProcesses.remove((Integer)i);
-						pwd();
-					}
-				}*/
 				error=false;
 				String input=readInput();
 				m=commandPattern.matcher(input);
@@ -109,7 +98,12 @@ public class Shell implements Runnable {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * Quand une commande a fini cette méthode
+	 * permet au Shell de savoir
+	 * @param c, la commande qui a terminé
+	 * @param error, true si'il y avait une erreur
+	 */
 	public void notifyFinished(Command c, boolean error){
 		if(!error){
 			System.out.println("La commande pid="+c.getPID()+" a terminé, son resultat:");
@@ -132,7 +126,12 @@ public class Shell implements Runnable {
 		return input;
 	}
 	
-	
+	/**
+	 * Si dans l'execution d'une commande on reconte une erreur
+	 * cette methode nous permet d'afficher une message et 
+	 * arreter l'execution
+	 * @param message
+	 */
 	public void showErrorMessage(String message){
 		error=true;
 		System.out.println("Erreur: "+message);
